6963b55fdc2ffbb1aa9797442b414eb8904385b6
mr-GreyWolf <mr.greywolf@list.ru>
2019-08-28 10:40:04 +0300

    update font-to-text.py

--- font/font-to-text.py
+++ font/font-to-text.py
@@ -12,19 +12,16 @@ if __name__ == '__main__':
 	sys.exit (1)
 
 # Переменные
-offset = "1BBF"
-length = 2048
+offset = "1BBF"		# смещение
+length = 2048		# длина
 table = []; 		# таблица
-sep=" "			# разделитель
 line_end = "\r\n"	# конец строки
+replace_0 = ' ' # замена для 0
+replace_1 = '@' # замена для 1
 
 # Чтение исходного файла
 file_in = open(file_i, 'rb')
 data_out = ''
-replace_0 = ' ' # Замена 0
-replace_1 = '@' # Замена 1
-
-
 
 file_in.seek( int(offset, base = 16) )
 data_in = file_in.read(1)
@@ -33,29 +30,23 @@ while length:
     code_hex = code_hex.upper()
     table.append(code_hex);
 
-#   string_of_pixels=((bin(int(code_hex, 16))[2:].zfill(8)).replace('0', replace_0)).replace('1', replace_1)
-#   print str(test1)
-
     length = length-1
     data_in = file_in.read(1)
 file_in.close()
 
-#print table[2047]
-#sys.exit (0)
-
 # Шрифт
 s=0
 for i in range (0,2047,8):
-    i_hex = '{0:x}'.format(int(s))
-    i_hex = i_hex.upper()
+    i_hex = ('{0:x}'.format(int(s))).upper()
+    s_hex = ('{0:x}'.format(int(s*8))).upper()
     if s <= 16:
-	i_hex = '0x0'+ i_hex
+	i_hex = '0x0' + i_hex
     else:
-	i_hex = '0x'+ i_hex
-    data_out =  data_out + i_hex + ' (' + str(s) + ')' + line_end + \
+	i_hex = '0x' + i_hex
+    data_out =  data_out + 'Смещение: '+ '0x' + str(s_hex)+ ' ('  + str (s*8)+ ')   Код: ' + i_hex + ' (' + str(s) + ')' + line_end + \
     '_12345678' + line_end
     for t in range (0,8):
-        string_of_pixels=((bin(int(table[i+t], 16))[2:].zfill(8)).replace('0', replace_0)).replace('1', replace_1)
+	string_of_pixels=((bin(int(table[i+t], 16))[2:].zfill(8)).replace('0', replace_0)).replace('1', replace_1)
 	data_out = data_out + str (t+1) + string_of_pixels + line_end
     data_out =  data_out + line_end
     s=s+1
