31385fb05767130f17b98bd65eab30464c36454a
mr-GreyWolf <mr.greywolf@list.ru>
2019-12-06 13:29:25 +0300

    Add files via upload

--- converter/msx_russian-utf8-batch_mode.sh
+++ converter/msx_russian-utf8-batch_mode.sh
@@ -9,8 +9,8 @@ file_ext_utf='.txt'	# расширение конечного файла (UTF-8)
 
 file_msx_mask=$file_msx_mask$file_ext_msx
 for file_msx in `find $files_path -type f -name "$file_msx_mask"`
-    do
-	file_utf=${file_msx//$file_ext_msx/$file_ext_utf}
-	python ./msx_russian-utf8.py $file_msx $file_utf
-    done
+	do
+		file_utf=${file_msx//$file_ext_msx/$file_ext_utf}
+		python ./msx_russian-utf8.py $file_msx $file_utf
+	done
 exit
