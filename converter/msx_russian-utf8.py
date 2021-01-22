7b6e97ac5a057a377df7b892f3dcc9690feb8255
mr-GreyWolf <mr.greywolf@list.ru>
2021-01-22 10:17:16 +0300

    Update msx_russian-utf8.py

--- converter/msx_russian-utf8.py
+++ converter/msx_russian-utf8.py
@@ -186,7 +186,7 @@ prefix=0
 
 while data_in:
 	code_dec=ord(data_in)
-	code_hex=(binascii.b2a_hex(data_in)).decode("ascii")
+	code_hex=(binascii.b2a_hex(data_in)).decode('ascii')
 	data_in=file_in.read(1)
 	code_bin=binascii.unhexlify(code_hex)
 
